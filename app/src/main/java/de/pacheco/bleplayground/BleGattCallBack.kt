99eb09e3b0888e62d9eb8e178b89bdd5ad071700
pacheco <pacheco@gmx.de>
2021-10-03 23:00:37 +0200

    Added BleGattCallBack.kt class

---
+++ app/src/main/java/de/pacheco/bleplayground/BleGattCallBack.kt
@@ -0,0 +1,56 @@
+package de.pacheco.bleplayground
+
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+
+class BleGattCallBack : BluetoothGattCallback() {
+    override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
+        LogHelper.printIt("onCharacteristicChanged")
+    }
+
+    override fun onCharacteristicRead(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
+        LogHelper.printIt("onCharacteristicRead")
+    }
+
+    override fun onCharacteristicWrite(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
+        LogHelper.printIt("onCharacteristicWrite")
+    }
+
+    override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+        LogHelper.printIt("onConnectionStateChange")
+    }
+
+    override fun onDescriptorRead(gatt: BluetoothGatt?, descriptor: BluetoothGattDescriptor?, status: Int) {
+        LogHelper.printIt("onDescriptorRead")
+    }
+
+    override fun onDescriptorWrite(gatt: BluetoothGatt?, descriptor: BluetoothGattDescriptor?, status: Int) {
+        LogHelper.printIt("onDescriptorWrite")
+    }
+
+    override fun onMtuChanged(gatt: BluetoothGatt?, mtu: Int, status: Int) {
+        LogHelper.printIt("onMtuChanged")
+    }
+
+    override fun onPhyRead(gatt: BluetoothGatt?, txPhy: Int, rxPhy: Int, status: Int) {
+        LogHelper.printIt("onPhyRead")
+    }
+
+    override fun onPhyUpdate(gatt: BluetoothGatt?, txPhy: Int, rxPhy: Int, status: Int) {
+        LogHelper.printIt("onPhyUpdate")
+    }
+
+    override fun onReadRemoteRssi(gatt: BluetoothGatt?, rssi: Int, status: Int) {
+        LogHelper.printIt("onReadRemoteRssi")
+    }
+
+    override fun onReliableWriteCompleted(gatt: BluetoothGatt?, status: Int) {
+        LogHelper.printIt("onReliableWriteCompleted")
+    }
+
+    override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+        LogHelper.printIt("onServicesDiscovered")
+    }
+}
